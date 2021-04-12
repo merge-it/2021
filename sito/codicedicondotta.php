5aca6619558cc8eac56a6e8107474c09614fec9b
Daniele Scasciafratte <mte90net@gmail.com>
2021-04-12 14:34:15 +0200

    fix(coc): ultima menzione sistemata

--- sito/codicedicondotta.php
+++ sito/codicedicondotta.php
@@ -17,7 +17,7 @@ page([
         "Note",
         "Le molestie includono commenti offensivi verbali legati a genere, orientamento sessuale, disabilità, aspetto fisico, dimensione corporea, razza, religione, immagini sessuali in spazi pubblici, intimidazioni intenzionali, stalking, inseguimenti, fotografie o registrazioni moleste, interruzione continuata di presentazioni o altri eventi, contatto fisico inappropriato e attenzioni sessuali sgradite. I partecipanti cui viene chiesto di interrompere qualsiasi comportamento molesto sono tenuti a conformarsi immediatamente.",
         "<b>Se vieni molestato/a, noti che qualcun altro/a viene molestato/a, o hai qualsiasi altra preoccupazione, contatta immediatamente un membro del personale della conferenza.</b>",
-        "Ringranziamo <a href='https://pycon.it/it/code-of-conduct'>Python Italia</a> per averci permesso di usare il loro CoC come base.",
+        "Ringranziamo <a href='https://pycon.it/it/code-of-conduct'>Python Italia</a> per averci permesso di usare il loro Codice di Condotta come base.",
         "Contatti di riferimento da definire una volta deciso i lteam di volontari."
     ]
 ]);
